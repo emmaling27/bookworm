3c62df41638e5cd426dea48a16b93f13d58823f4
Emma Forman Ling <emma@convex.dev>
2022-04-27 17:03:44 -0700

    gut channel and message code

--- app/convex/addChannel.ts
+++
@@ -1,7 +0,0 @@
-import { mutation } from "convex-dev/server";
-import { Id } from "convex-dev/values";
-
-// Create a new chat channel.
-export default mutation(({ db }, name: string): Id => {
-  return db.insert("channels", { name });
-});
--- app/src/common.ts
+++ app/src/common.ts
@@ -2,19 +2,6 @@ import { ConvexReactClient } from "convex-dev/react";
 import { Id } from "convex-dev/values";
 import convexConfig from "../convex.json";
 
-export type Message = {
-  _id: Id;
-  channel: Id;
-  body: string;
-  author: string;
-  time: number;
-};
-
-export type Channel = {
-  _id: Id;
-  name: string;
-};
-
 export const convex = new ConvexReactClient(convexConfig.origin);
 
 export type User = {
